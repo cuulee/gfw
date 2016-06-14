39a617ef5225df531a18788dd7f74e7596640f0d
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-06-14 11:52:07 +0200

    Steps in contact form.

--- app/assets/javascripts/static/ContactUsView.js
+++ app/assets/javascripts/static/ContactUsView.js
@@ -16,44 +16,52 @@ define([
     el: '#contactUsView',
 
     events: {
-      'submit form' : 'onFormSubmit',
       'change #contact-topic' : 'onTopicChange',
     },
 
     model: new (Backbone.Model.extend({
     	defaults : {
+    		step: 0,
     		topics: {
 					'report-a-bug-or-error-on-gfw': {
 						name: 'Report a bug or error on GFW',
-						placeholder: 'Explain the bug or error and tell us where on the website you encountered it. What browser (e.g., Chrome version 50.0.2661.94 m) and operating system (e.g., Windows 8.1) do you use?'
+						placeholder: 'Explain the bug or error and tell us where on the website you encountered it. What browser (e.g., Chrome version 50.0.2661.94 m) and operating system (e.g., Windows 8.1) do you use?',
+						emailTo: 'gfw@wri.org'
 					},
 					'provide-feedback': {
 						name: 'Provide feedback',
-						placeholder: 'Tell us about your experience with GFW! Examples: How can we improve GFW? Why did you visit GFW? How do you use GFW? If and how is the information provided by GFW useful for your work? Are there any additional features and/or data that would be useful?  Was anything confusing or difficult to use?  Etc...'
+						placeholder: 'Tell us about your experience with GFW! Examples: How can we improve GFW? Why did you visit GFW? How do you use GFW? If and how is the information provided by GFW useful for your work? Are there any additional features and/or data that would be useful?  Was anything confusing or difficult to use?  Etc...',
+						emailTo: 'gfw@wri.org'					
 					},
 					'media-request': {
 						name: 'Media request',
-						placeholder: 'How can we help you?'
+						placeholder: 'How can we help you?',
+						emailTo: 'gfwmedia@wri.org'						
 					},
 					'data-related-inquiry': {
 						name: 'Data related inquiry',
-						placeholder: 'How can we help you?'
+						placeholder: 'How can we help you?',
+						emailTo: 'gfwdata@wri.org'						
 					},
 					'gfw-commodities-inquiry': {
 						name: 'GFW Commodities inquiry',
-						placeholder: 'How can we help you?'
+						placeholder: 'How can we help you?',
+						emailTo: 'gfwcommodities@wri.org'						
 					},
 					'gfw-fires-inquiry': {
 						name: 'GFW Fires inquiry',
-						placeholder: 'How can we help you?'
+						placeholder: 'How can we help you?',
+						emailTo: 'gfwfires@wri.org'						
 					},
 					'gfw-climate-inquiry': {
 						name: 'GFW Climate inquiry',
-						placeholder: 'How can we help you?'
+						placeholder: 'How can we help you?',
+						emailTo: 'gfwclimate@wri.org'						
 					},
 					'general-inquiry': {
 						name: 'General inquiry',
-						placeholder: 'How can we help you?'
+						placeholder: 'How can we help you?',
+						emailTo: 'gfw@wri.org'						
 					},    		
     		}
     	}
@@ -66,18 +74,28 @@ define([
       this.cache();
       this.listeners();
       this.renderChosen();
-      this.validateForm();
+      this.validateContactForm();
+      this.validateNewsletterForm();
     },
 
     cache: function() {
-    	this.$contactForm = this.$el.find('#contact-form');
-    	this.$contactMessage = this.$el.find('#contact-message');
-    	this.$contactTopic = this.$el.find('#contact-topic');
     	this.$select = this.$el.find('.chosen-select');
+    	// Steps
+    	this.$steps = this.$el.find('.step');
+
+    	// Contact
+    	this.$contactForm = this.$el.find('#contact-form');
+    	this.$contactTopic = this.$el.find('#contact-topic');
+    	this.$contactMessage = this.$el.find('#contact-message');
+    	this.$contactEmailTo = this.$el.find('#contact-emailto');
+
+    	// Newsletter
+			this.$newsletterForm = this.$el.find('#newsletter-form');    	
     },
 
     listeners: function() {
     	this.model.on('change:topic', this.topicChanged.bind(this));
+    	this.model.on('change:step', this.stepChanged.bind(this));
     },
 
     // Plugins & helpers    
@@ -91,7 +109,7 @@ define([
       });
     },
 
-    validateForm: function() {
+    validateContactForm: function() {
 			this.$contactForm.validate({
 				ignore: ".ignore",
 				rules: {
@@ -125,8 +143,75 @@ define([
 			  },
 
 			  submitHandler: function(form) {
+					var formData = _.object($(form).serializeArray().map(function(v) {
+						return [v.name, v.value];
+					}));
+					console.log(formData);
+
+					// Send via ajax the data needed...where?
+					if (true) {
+						this.model.set('step', 1)
+					} else {
+
+					}
 			  	return false;
-			  }
+			  }.bind(this)
+			});
+    },
+
+    validateNewsletterForm: function() {
+			this.$newsletterForm.validate({
+				ignore: ".ignore",
+				rules: {
+					// no quoting necessary
+					'newsletter-email': {
+						required: true,
+						email: true
+					},
+					'newsletter-name': {
+						required: true,
+					},
+					'newsletter-city': {
+						required: true,
+						minlength: 20,
+					},
+					'newsletter-country': {
+						required: true,
+						minlength: 20,
+					},
+
+				},
+
+				messages: {
+			    'newsletter-email': {
+			      required: "This field is required",
+			      email: "Your email address must be in the format of name@domain.com"
+			    },
+					'newsletter-name': {
+			      required: "This field is required",
+					},
+			    'newsletter-city': {
+			      required: "This field is required",
+			    },		    
+			    'newsletter-country': {
+			      required: "This field is required",
+			    }			    
+			  },
+
+			  submitHandler: function(form) {
+					var formData = _.object($(form).serializeArray().map(function(v) {
+						return [v.name, v.value];
+					}));
+					console.log(formData);
+
+					// Send via ajax the data needed...where?
+					if (true) {
+						this.model.set('step', 2);
+					} else {
+
+					}
+			  	return false;
+			  }.bind(this)
 			});
     },
 
@@ -136,21 +221,26 @@ define([
     topicChanged: function() {
     	var topic = this.model.get('topic');
     	var placeholder = this.model.get('topics')[topic]['placeholder'];
+    	var emailTo = this.model.get('topics')[topic]['emailTo'];
+
     	this.$contactMessage.attr('placeholder', placeholder);
+    	this.$contactEmailTo.val(emailTo);
     },
 
-    /**
-     * UI EVENTS
-     */
-    onFormSubmit: function(e){
-      e && e.preventDefault();
-      console.log('form submit');
+    stepChanged: function() {
+    	var step = this.model.get('step');
+    	var $currentStep = this.$steps.filter(function(){
+				return $(this).data("step") == step;
+			});
+
+    	this.$steps.toggleClass('-active', false);
+    	$currentStep.toggleClass('-active', true);
     },
 
     onTopicChange: function(e) {
       e && e.preventDefault();
       var topic = $(e.currentTarget).val();
-      this.model.set('topic', topic)
+      this.model.set('topic', topic);
     },
 
   });
